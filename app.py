e8e4f68ce37b765523f555905ca5e7533b66edb1
akshays236 <akshays23623@gmail.com>
2025-03-29 23:55:29 +0530

    Add CRUD operations for todos and handle not found errors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 
@@ -16,5 +16,38 @@ todos = []
 
 @app.post("/todos")
 async def add_todos(todo: Todo):
+    todo.id = len(todos) + 1
     todos.append(todo)
     return todo
+
+
+@app.get("/todos")
+async def read_todos():
+    return todos
+
+
+@app.get("/todos/{id}")
+async def read_todo(id: int):
+    for todo in todos:
+        if todo.id == id:
+            return todo
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.put("/todos/{id}")
+async def update_todo(id: int, new_todo: Todo):
+    for index, todo in enumerate(todos):
+        if todo.id == id:
+            todos[index] = new_todo
+            todos[index].id = id
+            return new_todo
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.delete("/todos/{id}")
+async def delete_todo(id: int):
+    for index, todo in enumerate(todos):
+        if todo.id == id:
+            del todos[index]
+            return
+    raise HTTPException(status_code=404, detail="Todo not found")
